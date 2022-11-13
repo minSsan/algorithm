9d3e9e22a2abc15103f6050ce1304bfb31024143
minSsan <msp1125@hanyang.ac.kr>
2022-11-13 15:37:19 +0900

    Solved: heap - 2075

--- baekjoon/ive_coding/algorithm_lecture/heap/2075.cpp
+++ baekjoon/ive_coding/algorithm_lecture/heap/2075.cpp
@@ -4,19 +4,36 @@
 using namespace std;
 
 int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
     int n;
+    cin >> n;
+
     int nums[n][n];
 
-    priority_queue<int> pq;
-    // 첫 번째 최대 값 : 마지막 줄에 있는 수
-    // 두 번째 최대 값 : ...
+    priority_queue<pair<int, pair<int, int>>> pq;
     
-
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cin >> nums[i][j];
         }
     }
+
+    for (int j = 0; j < n; ++j){
+        pq.push({nums[n-1][j], {n-1, j}});
+    }
+
+    int row, col;
+    for (int i = 0; i < n-1; ++i) {
+        row = pq.top().second.first - 1;
+        col = pq.top().second.second;
+        pq.push({nums[row][col], {row, col}});
+        pq.pop();
+    }
+
+    cout << nums[pq.top().second.first][pq.top().second.second] << '\n';
 
     return 0;
 }
