7d9ebec2ca42c51726941a9f1a216b23fe2e6477
minSsan <msp1125@hanyang.ac.kr>
2022-12-10 22:34:41 +0900

    Solved: 11866 - queue

---
+++ baekjoon/ive_coding/algorithm_lecture/queue/11866.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n, k;
+    queue<int> q;
+    vector<int> result;
+
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; ++i) {
+        q.push(i);
+    }
+
+    int temp;
+
+    while (!q.empty()) {
+        for (int i = 0; i < k-1; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+        result.push_back(q.front());
+        q.pop();
+    }
+
+    cout << '<';
+
+    for (int i = 0; i < n - 1; ++i) {
+        cout << result[i] << ", ";
+    }
+
+    cout << result[n-1] << ">\n";
+
+    return 0;
+}
