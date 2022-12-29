2394f6625ccce84fe9f61a502a14ab6e17deb440
minSsan <msp1125@hanyang.ac.kr>
2022-12-29 16:19:30 +0900

    Solved: 1213 - implement

---
+++ algorithm_study/week3-implement/1213.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int cnt[26] = {0};
+    string text;
+    cin >> text;
+
+    for (char c : text) {
+        cnt[c - 'A']++;
+    }
+
+    string center = "";
+    string front;
+    string back;
+    bool exist = true;
+
+    for (int i = 0; i < 26; ++i) {
+        if (cnt[i] == 0) continue;
+
+        if (cnt[i] % 2 == 0) {
+            while (cnt[i] != 0) {
+                front.push_back(char(i + 'A'));
+                back = char(i + 'A') + back;
+                cnt[i] -= 2;
+            }
+        } else if (cnt[i] % 2 == 1 && center == "") {
+            center = char(i + 'A');
+            cnt[i]--;
+            while (cnt[i] != 0) {
+                front.push_back(char(i + 'A'));
+                back = char(i + 'A') + back;
+                cnt[i] -= 2;
+            }
+        } else {
+            exist = false;
+        }
+    }
+
+    if (!exist) {
+        cout << "I'm Sorry Hansoo\n"; 
+    } else {
+        cout << front + center + back << '\n';
+    }
+
+    return 0;
+}
