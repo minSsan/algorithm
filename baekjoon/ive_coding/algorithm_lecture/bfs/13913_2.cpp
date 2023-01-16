92a652edc25b1f1588066e542c8dfd373d075323
minSsan <msp1125@hanyang.ac.kr>
2023-01-16 20:49:06 +0900

    Update: 13913 - visited 없이

---
+++ baekjoon/ive_coding/algorithm_lecture/bfs/13913_2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+queue<int> q;
+
+const int MAX = 100000;
+int prev_loc[100001]; // 인덱스 값의 위치를 방문하기 전에 거쳐온 위치를 저장
+
+void bfs(int start, int end) {
+    prev_loc[start] = -1;
+    q.push(start);
+
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+
+        if (now == end) {
+            return ;
+        }
+
+        if (now + 1 <= MAX && prev_loc[now + 1] == -100) {
+            prev_loc[now + 1] = now;
+            q.push(now + 1);
+        }
+
+        if (now - 1 >= 0 && prev_loc[now - 1] == -100) {
+            prev_loc[now - 1] = now;
+            q.push(now - 1);
+        }
+
+        if (2 * now <= MAX && prev_loc[2 * now] == -100) {
+            prev_loc[2 * now] = now;
+            q.push(2 * now);
+        }
+    }
+}
+
+int main() {
+    for (int i = 0; i < 100001; ++i) {
+        prev_loc[i] = -100;
+    }
+
+    int n, k;
+    cin >> n >> k;
+
+    bfs(n, k);
+
+    vector<int> s;
+    int index = k;
+    int cnt = 0;
+    while (index != -1) {
+        cnt++;
+        s.push_back(index);
+        index = prev_loc[index];
+    }
+
+    cout << --cnt << '\n';
+    
+    for (int i = s.size() - 1; i >= 0; --i) {
+        cout << s[i] << ' ';
+    }
+
+    cout << '\n';
+    
+    return 0;
+}
