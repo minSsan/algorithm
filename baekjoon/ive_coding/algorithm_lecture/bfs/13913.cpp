fdde7e7746ce0893580cd4e3fb3ae8ef59d2b18d
minSsan <msp1125@hanyang.ac.kr>
2023-01-16 14:16:40 +0900

    Solved: 13913 - bfs

--- baekjoon/ive_coding/algorithm_lecture/bfs/13913.cpp
+++ baekjoon/ive_coding/algorithm_lecture/bfs/13913.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <queue>
-#include <deque>
 #include <vector>
 
 using namespace std;
 
-deque<int> dq;
+queue<int> q;
 
 const int MAX = 100000;
 int prev_loc[100001]; // 인덱스 값의 위치를 방문하기 전에 거쳐온 위치를 저장
+bool visited[100001];
 
 void bfs(int start, int end) {
     prev_loc[start] = -1;
-    dq.push_back(start);
+    q.push(start);
 
-    while (!dq.empty()) {
-        int now = dq.front();
-        dq.pop_front();
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
 
         if (now == end) {
             return ;
         }
 
-        if (now + 1 <= MAX && prev_loc[now + 1] == 0) {
+        if (now + 1 <= MAX && prev_loc[now + 1] == 0 && !visited[now + 1]) {
             prev_loc[now + 1] = now;
-            dq.push_back(now + 1);
+            visited[now + 1] = true;
+            q.push(now + 1);
         }
 
-        if (now - 1 >= 0 && prev_loc[now - 1] == 0) {
+        if (now - 1 >= 0 && prev_loc[now - 1] == 0 && !visited[now - 1]) {
             prev_loc[now - 1] = now;
-            dq.push_back(now - 1);
+            visited[now - 1] = true;
+            q.push(now - 1);
         }
 
-        if (2 * now <= MAX && prev_loc[2 * now] == 0) {
+        if (2 * now <= MAX && prev_loc[2 * now] == 0 && !visited[2 * now]) {
             prev_loc[2 * now] = now;
-            dq.push_back(2 * now);
+            visited[2 * now] = true;
+            q.push(2 * now);
         }
     }
 }
@@ -45,23 +48,19 @@ int main() {
 
     bfs(n, k);
 
-    while (!dq.empty()) {
-        dq.pop_back();
-    }
-
+    vector<int> s;
     int index = k;
     int cnt = 0;
     while (index != -1) {
         cnt++;
-        dq.push_front(index);
+        s.push_back(index);
         index = prev_loc[index];
     }
 
     cout << --cnt << '\n';
     
-    while (!dq.empty()) {
-        cout << dq.front() << ' ';
-        dq.pop_front();
+    for (int i = s.size() - 1; i >= 0; --i) {
+        cout << s[i] << ' ';
     }
 
     cout << '\n';
