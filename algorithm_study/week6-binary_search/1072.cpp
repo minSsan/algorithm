6f133c1eec5b136f0ddf9f1bac09baed837a2783
minSsan <msp1125@hanyang.ac.kr>
2023-01-18 19:13:46 +0900

    Solved: 1072 - binary search

--- algorithm_study/week6-binary_search/1072.cpp
+++ algorithm_study/week6-binary_search/1072.cpp
@@ -30,47 +30,44 @@ int main() {
 
     // cout << "z: " << z << '\n';
 
-    vector<int> win_cnt(x - y); // 현재까지 진 횟수 만큼
-    for (int i = 0; i < win_cnt.size(); ++i) {
-        win_cnt[i] = i + 1;
-    }
-
-    int start = 0, end = win_cnt.size() - 1;
+    int start = 1, end = x;
     int mid;
     int min_cnt = 1000000000; // 조건을 만족하는 현재까지의 최소 이긴 횟수
-    bool flag = false;
     int min_odds = 100; // 조건을 만족하는 현재까지의 최소 승률 값
     int current_odds;
 
+    tmp = (y + end) * 100;
+    if (tmp / (x + end) == z) { // 아무리 이겨도 승률을 올릴 수 없다면
+        cout << -1 << '\n'; // -1 출력 후 종료
+        return 0;
+    }
+
     while (start <= end) {
         mid = (start + end) / 2;
-        tmp = (y + win_cnt[mid]) * 100;
-        current_odds = tmp / (x + win_cnt[mid]);
+        tmp = (y + mid) * 100;
+        current_odds = tmp / (x + mid);
         // cout << "mid: " << mid << '\n';
         // cout << "current_odds: " << current_odds << '\n';
         // cout << "min_odds: " << min_odds << '\n';
         
         if (current_odds > z) { // z보다 큰 승률이 나왔을 때
-            if (current_odds <= min_odds) { // 현재까지의 최소 승률보다 작거나 같을 때
-                min_odds = current_odds; // 최소 승률을 갱신
-                if (win_cnt[mid] < min_cnt) { // 현재 이긴 횟수가 최소 이긴 횟수보다 작을 때
-                    flag = true;
-                    min_cnt = win_cnt[mid];
-                }
-            }
+            min_cnt = mid;
+            end = mid - 1;
+            // if (current_odds <= min_odds) { // 현재까지의 최소 승률보다 작거나 같을 때
+            //     min_odds = current_odds; // 최소 승률을 갱신
+            //     if (mid < min_cnt) { // 현재 이긴 횟수가 최소 이긴 횟수보다 작을 때
+            //         min_cnt = mid;
+            //     }
+            // }
 
-            if (current_odds >= min_odds) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+            // if (current_odds >= min_odds) {
+            //     end = mid - 1;
+            // } else {
+            //     start = mid + 1;
+            // }
         } else { // z랑 동일한 승률이 나왔을 때 -> 별도의 작업 필요 없이 재탐색
             start = mid + 1;
         }
-    }
-
-    if (!flag) {
-        min_cnt = -1;
     }
 
     cout << min_cnt << '\n';
