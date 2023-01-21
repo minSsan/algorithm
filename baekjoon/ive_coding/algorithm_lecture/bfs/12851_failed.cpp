bf4019116f4d66aec8c8ac63592dc12d70993a32
minSsan <msp1125@hanyang.ac.kr>
2023-01-21 18:48:13 +0900

    Add: 12851 - failed code

--- baekjoon/ive_coding/algorithm_lecture/bfs/12851_failed.cpp
+++ baekjoon/ive_coding/algorithm_lecture/bfs/12851_failed.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,10 +16,15 @@ int min_cnt; // 최소 경로 수로 이동할 수 있는 경우의 수
 
 // ? 최소 경로를 먼저 찾는다.
 // ? 최소 경로를 찾았으면 isFound를 true로 변경하고, 그 뒤에 이어지는 가지는 푸시하지 X
+// ! 방문 처리 시점에 따라서 방문 횟수가 달라진다.
+// -> 만약 서로 다른 경로를 모두 구하는 경우에는, 큐에 푸시함과 동시에 방문 처리를 하면 안된다. 이 경우에는 큐에 pop하면서 방문처리를 해야 함
+// -> 하지만 이런 특수한 경우가 아니라면 큐에 푸시함과 동시에 방문 처리를 하는 것이 일반적이다.
+// -> 그렇지 않으면 이미 방문한 노드를 또 다시 큐에 푸시하기 때문에, 메모리 초과가 발생할 수 있다.
 
 void bfs(int start) {
     q.push(start);
     visited[start] = true;
+    cnt[start] = 0;
 
     while (!q.empty()) {
         int now = q.front();
@@ -27,47 +33,46 @@ void bfs(int start) {
         cout << "now: " << now << '\n';
 
         if (now == k) {
-            cout << "cnt[k]: " << cnt[k] << '\n';
-            if (!isFound) { // ? 아직 최소 경로를 찾기 전이라면,
+            if (!isFound) {
+                cout << "cnt[k]: " << cnt[k] << '\n';
+                
                 isFound = true; // ? 최소값 찾음 표시 남기기
                 min_result = cnt[k]; // ? 최소 경로 수 저장
                 min_cnt = 1;
-            } 
-            visited[k] = false; // ? 다른 경로도 찾기 위해서 방문처리 해제
+                
+                visited[k] = false; // ? 다른 경로도 찾기 위해서 방문처리 해제
+            } else if (cnt[k] == min_result) {
+                min_cnt++;
+            }
         } 
 
         if (now - 1 >= 0 && !visited[now - 1]) {
             if (!isFound) { // ? 아직 최소값이 발견되지 않았다면
-                cnt[now - 1] = cnt[now] + 1;
+                cout << "now found" << '\n';
+                cnt[now - 1] = min(cnt[now - 1], cnt[now] + 1);
                 q.push(now - 1);
-            } else if (now - 1 == k) { // ? 최소값이 발견된 상태고, now - 1 값이 k라면,
-                if (cnt[now] + 1 == min_result) {
-                    min_cnt++;
-                }
+            } else {
+                cnt[now - 1] = cnt[now] + 1;
             }
         }
 
         if (now + 1 <= MAX && !visited[now + 1]) {
             if (!isFound) { // 아직 최소값이 발견되지 않았다면
+                cout << "now found" << '\n';
+                cnt[now + 1] = min(cnt[now + 1], cnt[now] + 1);
+                q.push(now + 1);  
+            } else {
                 cnt[now + 1] = cnt[now] + 1;
-                q.push(now + 1);
-                
-            } else if (now + 1 == k) {
-                if (cnt[now] + 1 == min_result) {
-                    min_cnt++;
-                }
             }
         }
 
         if (2 * now <= MAX && !visited[2 * now]) {
             if (!isFound) { // 아직 최소값이 발견되지 않았다면
+                cout << "now found" << '\n';
+                cnt[2 * now] = min(cnt[2 * now], cnt[now] + 1);
+                q.push(2 * now); 
+            } else {
                 cnt[2 * now] = cnt[now] + 1;
-                q.push(2 * now);
-                
-            } else if (2 * now == k) {
-                if (cnt[now] + 1 == min_result) {
-                    min_cnt++;
-                }
             }
         }
     }
@@ -75,6 +80,10 @@ void bfs(int start) {
 
 int main() {
     cin >> n >> k;
+
+    for (int i = 0; i < 100001; ++i) {
+        cnt[i] = 100000;
+    }
 
     bfs(n);
 
