3998a5ee32a2d0ce1de4cc172e0e95f66de38076
minSsan <msp1125@hanyang.ac.kr>
2023-04-04 21:52:55 +0900

    Inprogress: 2343

---
+++ algorithm_study/week6-binary_search/2343.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// * n개의 영상을 m개로 나누었을 때, 최대 합이 최소가 되게끔
+
+int main() {
+    int lectures[100000];
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> lectures[i];
+    }
+
+    return 0;
+}
